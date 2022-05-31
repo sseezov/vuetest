f82211dfd9f37305e8325f4d077f13645fbdd3ac
sseezov <sseezov@mail.ru>
2022-05-31 20:16:38 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div><div >Количество лайков:{{ likes }} </div>
+    <button @click="like">like</button>
+    <button @click="dislike">dislike</button>
+    
+    </div>
+
+  
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+    }
+  },
+  methods: {
+    like(){
+      this.likes++
+    },
+    dislike(){
+      this.likes--
+    }
+  }
+};
+</script>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
